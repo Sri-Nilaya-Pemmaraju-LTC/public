47a046f62f5d15b5a02804bbdea0249dddb96783
Sri-Nilaya-Pemmaraju-LTC <156900544+Sri-Nilaya-Pemmaraju-LTC@users.noreply.github.com>
2024-05-23 15:08:49 +0530

    First pydeequ code commit

--- data_Quality_Project/main_module/pydeequ_checks.py
+++ data_Quality_Project/main_module/pydeequ_checks.py
@@ -1,20 +1,32 @@
+import os
+os.environ['PYARROW_IGNORE_TIMEZONE'] = '1'
+from pydeequ.checks import *
 import pandas as pd
-from pydeequ.analyzers import *
-from pydeequ.verification import VerificationSuite
-from pyspark.sql import SparkSession
+from pydeequ.verification import *
+from pyspark.sql import SparkSession  
+import pydeequ
+
 
 # Initialize Spark session
-spark = SparkSession.builder.appName("DataQualityChecks").getOrCreate()
+spark = (SparkSession
+.builder
+.config("spark.jars.packages", pydeequ.deequ_maven_coord)
+.config("spark.jars.excludes", pydeequ.f2j_maven_coord)
+.getOrCreate())
+
+
 
 # Read the CSV file from your desktop
-csv_file = '/path/to/your/desktop/customer_details.csv'
+csv_file = "data_Quality_Project/main_module/customer_details.csv"
 df = pd.read_csv(csv_file)
+spark_df = spark.createDataFrame(df)
+check = Check(spark, CheckLevel.Warning, "Review Check")
 
-# Create a PyDeequ DataFrame
-deequ_df = VerificationSuite(spark) \
-    .onData(df) \
+
+checkResult = VerificationSuite(spark) \
+    .onData(spark_df) \
     .addCheck(
-        Check(spark)
+        check.hasSize(lambda x: x >= 3)\
         .isComplete("name")  # Test Case 1: Verify that the name field is not empty
         .isComplete("account_number")  # Test Case 2: Ensure account number is not empty
         .isComplete("date_of_birth")  # Test Case 3: Ensure date of birth is not empty
@@ -24,9 +36,9 @@ deequ_df = VerificationSuite(spark) \
     ) \
     .run()
 
-# Print the results
-for result in deequ_df.checkResults():
-    print(f"Check '{result.check().name()}' passed: {result.status() == CheckStatus.Success}")
+checkResult_df = VerificationResult.checkResultsAsDataFrame(spark, checkResult)
+checkResult_df.show()
+
 
 # Stop the Spark session
 spark.stop()
